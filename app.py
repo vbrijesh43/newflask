256a71dd23ee8e82bd30bde4c86fe74087c3ad8f
vbrijesh43 <vbrijesh43@gmail.com>
2026-03-06 23:47:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Flask app is live 🚀"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
