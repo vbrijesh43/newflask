206ba1316d9c45140e284ee749bd119b650879d5
vbrijesh43 <vbrijesh43@gmail.com>
2026-03-07 13:50:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import math
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+USERNAME = "Vbrijesh"
+PASSWORD = "123Bri"
+
+@app.route("/", methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == USERNAME and password == PASSWORD:
+            return redirect(url_for("welcome", user=username))
+        else:
+            return "Invalid Username or Password"
+
+    return render_template("login.html")
+
+
+@app.route("/welcome/<user>", methods=["GET","POST"])
+def welcome(user):
+
+    result = None
+
+    if request.method == "POST":
+        number = float(request.form["number"])
+        result = math.sqrt(number)
+
+    return render_template("welcome.html", username=user, result=result)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
